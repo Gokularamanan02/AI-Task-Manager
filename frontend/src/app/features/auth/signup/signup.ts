9b093bafc7af21ec275a629db0987ea1d364bf12
Gokularamanan02 <gokulramanan166@gmail.com>
2026-07-24 23:19:05 +0530

    Fixed signup and deployment

--- frontend/src/app/features/auth/signup/signup.ts
+++ frontend/src/app/features/auth/signup/signup.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-imports: [FormsModule, RouterLink],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
@@ -17,12 +23,56 @@ export class Signup {
     password: ''
   };
 
+  errorMessage = '';
+  loading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
   signup() {
 
-    console.log("Signup Data:", this.signupData);
-    
+    this.errorMessage = '';
+
+    if (
+      !this.signupData.name ||
+      !this.signupData.email ||
+      !this.signupData.password
+    ) {
+      this.errorMessage = 'All fields are required';
+      return;
+    }
+
+    this.loading = true;
+
+    this.authService.signup(this.signupData).subscribe({
+
+      next: (response) => {
+
+        console.log("Signup Success:", response);
+
+        this.loading = false;
+
+        alert("Signup Successful");
+
+        this.router.navigate(['/login']);
+
+      },
+
+      error: (error) => {
+
+        console.log(error);
+
+        this.loading = false;
+
+        this.errorMessage =
+          error.error?.message || "Signup failed";
+
+      }
+
+    });
 
   }
-  
 
 }
